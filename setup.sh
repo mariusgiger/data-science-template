0d128f83fd2a8e5c28529ba1cfda092420c9f003
Marius Giger <gigermarius@gmail.com>
2020-09-08 18:01:07 +0200

    adds git initialization

--- setup.sh
+++ setup.sh
@@ -18,9 +18,16 @@ cd $project_name
 pip3 install -U virtualenv
 virtualenv -p python3 .
 source ./bin/activate
+
 python3 -m pip install -U pip
 pip install -r requirements.txt
 pip install -U jupyter_contrib_nbextensions
+
 jupyter contrib nbextension install --sys-prefix
 jupyter nbextension enable code_prettify/autopep8
 ipython kernel install --user --name $project_name --display-name "Python ($project_name)"
+
+rm -rf .git
+git init
+git add .
+git commit -m "initial commit"
