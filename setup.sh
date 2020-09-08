a263deadb88b86af7e50543de93a814e195ff826
Marius Giger <gigermarius@gmail.com>
2020-09-08 17:48:00 +0200

    adds setup script

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+exit_err() {
+    echo "ERROR: $@" >&2
+    exit 1
+}
+
+echo "Enter project name:"
+read project_name
+
+if [ -z "$project_name" ]; then
+    exit_err "please provide a project name."
+fi
+
+git clone git@github.com:mariusgiger/data-science-template.git "$project_name"
+cd $project_name
+pip3 install -U virtualenv
+virtualenv -p python3 .
+source ./bin/activate
+python3 -m pip install -U pip
+pip install -r requirements.txt
+pip install -U jupyter_contrib_nbextensions
+jupyter contrib nbextension install --sys-prefix
+jupyter nbextension enable code_prettify/autopep8
+ipython kernel install --user --name $project_name --display-name "Python ($project_name)"
